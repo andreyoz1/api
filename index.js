91950a21cea4675fb6e77070d287e7a896cfc5bb
andreyoz1 <andrey.murilloo@gmail.com>
2025-04-28 17:49:55 -0500

    servidor

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import fs from 'fs'; //modulo de fs para leer y escribir archivos
+import bodyParser from 'body-parser'; //modulo para parsear el body de las peticiones
 
 const app = express(); //instancia de express
 
@@ -11,3 +13,15 @@ app.get('/',
     (req, res) => {    // ruta, peticion, respuesta
   res.send('Hello World!');
 });
+
+app.post(
+    "/create-user",
+    (req, res) => { //ruta, peticion, respuesta
+    //todo lo que tengo que hacer para crear el usuario
+    // valido el JWT
+    //verifico los datos de la req
+    //Envio los datos a la DB
+    //Envio un mensaje de exito o error
+    res.send('User created!'); //respuesta al cliente
+    }
+);
