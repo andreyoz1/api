cbca94f8a50a763a58afe29773dbb9ae7e74069f
andreyoz1 <andrey.murilloo@gmail.com>
2025-04-30 17:35:04 -0500

    cambios de carpetas

---
+++ back/api/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import fs from 'fs'; //modulo de fs para leer y escribir archivos
+import bodyParser from 'body-parser'; //modulo para parsear el body de las peticiones
+
+
+const app = express(); //instancia de express
+
+app.listen(3000, () => { //puerto 3000
+  console.log('Server is running on port 3000'); //mensaje de inicio
+});
+
+//crear endpoints
+//un endpoint es una ruta que el servidor escucha y responde a las peticiones
+//en este caso, la ruta es la raiz del servidor
+app.get('/', 
+    (req, res) => {    // ruta, peticion, respuesta
+  res.send('Hello World!');
+});
+
+
+app.post(
+    "/create-user",
+    (req, res) => { //ruta, peticion, respuesta
+    //todo lo que tengo que hacer para crear el usuario
+    // valido el JWT
+    //verifico los datos de la req
+    //Envio los datos a la DB
+    //Envio un mensaje de exito o error
+    res.send('User created!'); //respuesta al cliente
+    }
+);
